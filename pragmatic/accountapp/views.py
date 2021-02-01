c3b14eb2d61a4b84f98736a52a70b749362b3525
MINJUNYEONG <vmffkdlhb@hanmail.net>
2021-02-01 14:57:55 +0900

    apply decorator

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,36 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 
+has_ownership = [account_ownership_required, login_required]
 
+@login_required
 def hello_world(request):
-    if request.method == "POST":
-
-        temp = request.POST.get('hello_world_input')
-
-        return render(request, 'accountapp/helloworld.html', context={'text': temp})
-    else:
-        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!!!'})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!!!'})
 
 
 # reverse와 reverse_lazy 차이는 class와 def 에서 쓸 수 있냐 없냐의 차이
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -38,7 +46,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
